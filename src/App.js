0f1a709054c8dea1bc6d5f01adb4af606c1d9854
mamatha <mamata.barupati@gmail.com>
2022-08-14 03:15:24 +0000

    Commited keys i.e uniqueNo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import UserProfile from './components/UserProfile/index'
+import UserProfile from './components/UserProfile'
 import './App.css'
 
 const userDetailsList = [
   {
+    uniqueNo: 1,
     imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
     name: 'Esthar Howard',
     role: 'Software Developer',
   },
   {
+    uniqueNo: 2,
     imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
     name: 'Floyed Miles',
     role: 'Software Developer',
   },
   {
+    uniqueNo: 3,
     imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
     name: 'Jacob Jones',
     role: 'Software Developer',
   },
   {
+    uniqueNo: 4,
     imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
     name: 'Devon Lane',
     role: 'Software Developer',
@@ -29,7 +33,7 @@ const App = () => (
     <h1 className="title">Users</h1>
     <ul>
       {userDetailsList.map(eachItem => (
-        <UserProfile userDetails={eachItem} />
+        <UserProfile userDetails={eachItem} key={eachItem.uniqueNo} />
       ))}
     </ul>
   </div>
